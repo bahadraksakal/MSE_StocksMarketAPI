dc20db37608d92b579fbaf6584667a7085c99946
Bahadır Aksakal <72493865+bahadraksakal@users.noreply.github.com>
2023-10-27 16:11:03 +0300

    HangFire eklendi - bazı düzenlemeler yapıldı.

---
+++ HangFireApp/BackGroundJobRunGetStocksService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangFireApp
+{
+    public class BackGroundJobRunGetStocksService
+    {
+        public static int counter=0;
+
+        public void RunGetStocksService(string getStocksServicePath)
+        {
+            Process.Start(getStocksServicePath);
+            Console.WriteLine($"GetStocksService {counter}. kere çalışıyor");
+            Debug.WriteLine($"GetStocksService {counter}. kere çalışıyor");
+            counter++;
+        }
+
+    }
+}
---
+++ HangFireApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Hangfire;
+
+namespace HangFireApp
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string GetStocksServiceExePath = @"..\..\..\..\GetStocksService\\bin\\Debug\\net7.0\\GetStocksService.exe";
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=StockMarketDbHangFire;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            
+            BackGroundJobRunGetStocksService jobRunGetStocksService = new BackGroundJobRunGetStocksService();
+            
+            using (var server = new BackgroundJobServer())
+            {
+                //5 dakikada bir çalıştırmak için Hangfire.
+                RecurringJob.AddOrUpdate(() => jobRunGetStocksService.RunGetStocksService(GetStocksServiceExePath), "*/30 * * * * *");
+
+                Console.WriteLine("Hangfire arka plan işlemleri başlatıldı. Çıkmak için ENTER'a basın.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
